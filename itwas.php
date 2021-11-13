dd88a4a9f5fc72d75048c1f5a9239169ee0a7bb3
Maxim <maklev@mail.ru>
2021-11-13 13:15:04 +0300

    Add files via upload

---
+++ itwas.php
@@ -0,0 +1,52 @@
+<?php
+
+class TennisGame
+{
+    public $score = '';
+
+    public function getScore($player1Name, $player2Name, $p1Score, $p2Score)
+    {
+        if ($p1Score == $p2Score) {
+            switch ($p1Score) {
+                case 0;
+                    $this->score = 'Love-All';
+                    break;
+                case 1;
+                    $this->score = 'Fifteen-All';
+                    break;
+                case 2;
+                    $this->score = 'Thirty-All';
+                    break;
+                case 3;
+                    $this->score = 'Forty-All';
+                    break;
+                default:
+                    $this->score = 'Deuce';
+            }
+        } else if ($p1Score >= 4 || $p2Score >= 4) {
+            $minusResult = $p1Score - $p2Score;
+            if ($minusResult == 1) {
+                $this->score = 'Advantage ' . $player1Name;
+            } elseif ($minusResult == -1) {
+                $this->score = 'Advantage ' . $player2Name;
+            } elseif ($minusResult >= 2) {
+                $this->score = 'Win for ' . $player1Name;
+            } else {
+                $this->score = 'Win for ' . $player2Name;
+            }
+        } else {
+            for ($i = 1; $i < 3; $i++) {
+                if ($i == 1) {
+                    $tempScore = $p1Score;
+                } else {
+                    $this->score .= '-';
+                    $tempScore = $p2Score;
+                }
+                switch ($tempScore)
+                {
+
+                }
+            }
+        }
+    }
+}
